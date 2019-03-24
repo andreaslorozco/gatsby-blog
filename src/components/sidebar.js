07dcba557e21aa1a833bf826c13bf6afaa2b3ec7
Andreas Leimbach <me@andreaslorozco.com>
2019-03-24 17:36:37 -0600

    added fa icons to sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FontAwesome from 'react-fontawesome';
 
 import './sidebar.css';
 import profileImage from './../images/profile-picture-small.jpg';
@@ -10,12 +11,12 @@ const Sidebar = () => (
     <p id="my-description">Soy un Ingeniero en Sistemas que actualmente se desempeña como Front End Web Developer. Además disfruto desarrollando aplicaciones web full stack con React y NodeJS.</p>
 
     <div id="aside-links">
-        <a href="https://www.google.com/maps/place/Costa+Rica/" class="aside-link"><i class="fas fa-globe"></i> Costa Rica</a>
-        <a href="http://www.andreaslorozco.com" class="aside-link"><i class="fas fa-link"></i> Página Web</a>
-        <a href="https://www.twitter.com/andreaslorozco" class="aside-link"><i class="fab fa-twitter"></i> Twitter</a>
-        <a href="https://instagram.com/andreaslorozco/" class="aside-link"><i class="fab fa-instagram"></i>  Instagram</a>
-        <a href="https://github.com/andreaslorozco" class="aside-link"><i class="fab fa-github"></i> Github</a>
-        <a href="https://codepen.io/andreaslorozco/" class="aside-link"><i class="fab fa-codepen"></i>  Codepen</a>
+        <a href="https://www.google.com/maps/place/Costa+Rica/" class="aside-link"><FontAwesome name='globe'/> Costa Rica</a>
+        <a href="http://www.andreaslorozco.com" class="aside-link"><FontAwesome name='link'/> Página Web</a>
+        <a href="https://www.twitter.com/andreaslorozco" class="aside-link"><FontAwesome name='twitter'/> Twitter</a>
+        <a href="https://instagram.com/andreaslorozco/" class="aside-link"><FontAwesome name='instagram'/>  Instagram</a>
+        <a href="https://github.com/andreaslorozco" class="aside-link"><FontAwesome name='github'/> Github</a>
+        <a href="https://codepen.io/andreaslorozco/" class="aside-link"><FontAwesome name='codepen'/>  Codepen</a>
     </div>
     </aside>
 )
