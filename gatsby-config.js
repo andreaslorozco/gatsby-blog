1c04a5b9fa6b49a1a1233b9de8dbb6f31ff8570d
Leimbach Orozco, A <a.leimbach.orozco@accenture.com>
2020-04-02 07:44:43 -0600

    adds inline image support

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     "gatsby-plugin-catch-links",
     "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
     {
       resolve: `gatsby-source-filesystem`,
       options: { 
@@ -15,15 +16,18 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [], // just in case those previously mentioned remark plugins sound cool :)
-      },
-    },
-    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 490,
+            },
+          },
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
